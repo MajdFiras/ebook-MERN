943b7a37ecf99bb64117bb25e869432767b8b115
Majd Firas <majdfir4s@gmail.com>
2024-09-12 18:57:05 +0300

    Navbar UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
 import { Navbar } from "./components/Navbar"
-import { Body } from "./components/Body"
+import { Routes , Route } from "react-router-dom"
+import  Home  from "./pages/Home"
+import  Login  from "./pages/Login"
+import  SignUp  from "./pages/SignUp"
+import  Account  from "./pages/Account"
+
 
 function App() {
  
 
   return (
+   
     <>
-   <Navbar/>
-    <Body/>
+    
+    <Navbar/>
+      
+
+    
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/account" element={<Account/>} />
+    </Routes>
     </>
+    
+  
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
-
-
+import React from 'react';
+import { Flex, Box, Heading, UnorderedList, ListItem, Link } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <Box bg="gray.700" p="10" w="100%">
-       
+    <Box bg='#2a4365' color='white'>
+      <Flex
+        justify='space-between'
+        align='center'
+        px={8}
+        py={4}
+        height='80px'
+        maxW='1200px'
+        mx='auto'
+      >
+        <Heading as={RouterLink} to="/" fontSize='2xl' _hover={{ color: 'gray.400' }}>
+        &#128218; BuyBook
+        </Heading>
+        <UnorderedList display='flex' listStyleType='none' m={0} gap='20px'>
+          <ListItem>
+            <Link as={RouterLink} to="/login" _hover={{ textDecoration: 'underline', color: 'gray.400' }}>
+              Login
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link as={RouterLink} to="/signup" _hover={{ textDecoration: 'underline', color: 'gray.400' }}>
+              Signup
+            </Link>
+          </ListItem>
+        </UnorderedList>
+      </Flex>
     </Box>
-  )
-}
+  );
+};
