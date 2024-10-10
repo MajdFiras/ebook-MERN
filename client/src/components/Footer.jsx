081ea683d6bd912d48affd1ded8d50512ea023d3
Majd Firas <majdfir4s@gmail.com>
2024-10-10 23:31:17 +0300

    footer edit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import {Box , Text} from '@chakra-ui/react'
-import { FaGithub } from "react-icons/fa";
-
+import React from 'react';
+import { Box, Text, VStack, Link } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <Box width={'100%'} height={'300px'} backgroundColor={'#1e1e1e'} marginTop={'3%'}  >
-      <Box display={'flex'} justifyContent={'space-around'} padding={'5%'}>
-          <Box>
-            <Text fontSize={'3xl'} color={'white'}>PAPER</Text>
-          </Box> 
-          <Box >
-            <ul>
-              <li> <Text color={'white'}>Contact Us</Text> </li>
-              <li> <Text color={'white'}>FAQ</Text> </li>
-              <li><FaGithub color={'white'} width={'20px'} height={'20px'}/></li>
-            </ul>
-          </Box> 
+    <Box width="100%" backgroundColor="#1e1e1e" padding="3%" marginTop="3%">
+      <Box display="flex" justifyContent="space-between" alignItems="flex-start" paddingX="5%">
+        <Text fontSize="3xl" color="white">
+          PAPER
+        </Text>
+
+        <VStack alignItems="flex-start" spacing={0}>
+          <Link href="#" color="white" _hover={{ textDecoration: 'underline' }}>
+            Contact Us
+          </Link>
+          <Link href="#" color="white" _hover={{ textDecoration: 'underline' }}>
+            FAQ
+          </Link>
+          <Link href="https://github.com/MajdFiras/ebook-MERN" isExternal>
+            <FaGithub color="white" size="24px" />
+          </Link>
+        </VStack>
       </Box>
-
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
